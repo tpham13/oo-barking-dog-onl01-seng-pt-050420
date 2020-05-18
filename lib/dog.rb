8abe9c0059353a84d2a4a6699352cf0dfcbafcb9
Thanh Pham <tpham0123@gmail.com>
2020-05-18 17:53:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 # Your code goes here!
 class Dog 
-  def name=(dogs_name) 
-    @this_dogs_name = dogs_name 
-  end 
-end 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
 
 
