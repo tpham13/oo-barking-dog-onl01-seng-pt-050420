4439a4fb691a89c3cdb652398c1187d4f304cebd
Thanh Pham <tpham0123@gmail.com>
2020-05-18 17:49:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@
 class Dog 
 
   def name = (dogs_name) 
-    @this_dogs_name = dog_name 
+    @this_dogs_name = this_dogs_name 
   end 
   
   def name 
     @this_dogs_name
   end 
     
-  def dog_bark
-    dog.bark 
+  def dogs_bark
+    dogs.bark 
     puts "Woof"
   end 
   
