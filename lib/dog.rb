b50b196be34737b6b7e5da981d8a838a4e61c221
Thanh Pham <tpham0123@gmail.com>
2020-05-18 17:37:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,14 @@
 class Dog 
 
   def dog_name 
-    @dog_name = 
+    @dog_name = dog_new_name 
+  end 
+  
+  def dog_new_name
+    @dog_new_name
+    
+  def dog_bark
+    dog.bark 
+    puts "Woof"
 end 
+
